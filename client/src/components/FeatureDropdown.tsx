95a89e8167502c08f7f60bb6fe7ab11e782e3bb2
Zack Zellto <zacharyzellto@gmail.com>
2023-05-11 16:49:59 -0600

    Dashboard info style done on all viewports

--- client/src/components/FeatureDropdown.tsx
+++ client/src/components/FeatureDropdown.tsx
@@ -18,14 +18,18 @@ const CollapsibleCard: React.FC<CollapsibleCardProps> = ({
   return (
     <div
       onClick={toggleOpen}
-      className={`cursor-pointer transform transition hover:scale-110 w-[73%] border max-w-md mx-auto rounded-xl shadow-2xl overflow-hidden md:max-w-2xl m-4 bg-${backgroundColor}`}
+      className={`cursor-pointer transform transition hover:scale-110 lg:w-[45%] relative lg:left-[26%] bottom-[] w-[73%] border max-w-md mx-auto rounded-xl shadow-2xl overflow-hidden md:max-w-xl m-4 bg-${backgroundColor}`}
     >
       <div className="md:flex">
-        <div className="p-8">
-          <div className=" text-3xl text-center font-bold text-white">
+        <div className="p-4 m-auto">
+          <div className="text-3xl drop-shadow-lg font-bold text-center text-white">
             {title}
           </div>
-          {isOpen && <p className="mt-2 text-gray-500">{content}</p>}
+          {isOpen && (
+            <p className="mt-6 text-gray-500 text-center shadow-lg bg-white p-4 rounded-lg">
+              {content}
+            </p>
+          )}
         </div>
       </div>
     </div>
