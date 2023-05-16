7888c72ac978a953b29a31af72ffb98a2979e893
Zack Zellto <zacharyzellto@gmail.com>
2023-05-16 13:23:25 -0600

    added pricing cards, need to apply grid for lg:

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,9 +15,36 @@ function App() {
         <PricingInfo
           pricingTier="Free"
           price="0.00"
-          features={["Cash Quest!", "Dinnertime", "Groceries", "fmlyChat"]}
+          features={[
+            "Cash Quest!",
+            "Dinnertime",
+            "Groceries",
+            "fmlyChat",
+            "To-Do's",
+            "Calendar",
+          ]}
+          backgroundColor="bg-[#75BBE2]"
         />
+        <PricingInfo
+          pricingTier="Premium"
+          price="9.99"
+          features={[
+            "Cash Quest!",
+            "Dinnertime",
+            "Groceries",
+            "fmlyChat",
+            "To-Do's",
+            "Calendar",
+            "Find My fmly",
+            "bountyboard",
+          ]}
+          backgroundColor="bg-[#F59999]"
+        />
+        <div className="bg-white rounded-lg h-2 mt-24 w-[80%] m-auto relative"></div>
       </body>
+      <footer>
+        <div className="flex justify-center items-center mt-8">fmly, LLC.</div>
+      </footer>
     </div>
   );
 }
