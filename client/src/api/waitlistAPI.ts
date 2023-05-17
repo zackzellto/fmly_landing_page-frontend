8830c352439b34a85d3d090b39ed66499e6e68d6
Zack Zellto <zacharyzellto@gmail.com>
2023-05-16 21:08:35 -0600

    changed from axios to fetch

--- client/src/api/waitlistAPI.ts
+++ client/src/api/waitlistAPI.ts
@@ -1,9 +1,24 @@
-import axios from "axios";
-
 const baseURL = "https://fmly-landing-page-backend.herokuapp.com/api/waitlist";
 
 export const addToWaitlist = async (email: string) => {
-  const response = await axios.post(baseURL, { email });
-  console.log(response.data);
-  return response.data;
+  try {
+    const response = await fetch(baseURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to add email to waitlist");
+    }
+
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Error adding email to waitlist", error);
+    throw error;
+  }
 };
